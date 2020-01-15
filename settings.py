a4f525aeb965c257cc3754b47a8711f7c80741ad
brijesh <brijesh365@gmail.com>
2020-01-15 06:49:08 +0530

    hide api keys

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 # TODO load this settings from env
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
 RESULTS_COUNT = 5
-API_KEY = "AIzaSyDHVI8Pvfh5_bgNmZXVBs-EfOq0Sbr4ydk"
-CSE_ID = "001964664684553019863:8lmblvhbapf"
-DISCORD_TOKEN = 'NjY2NDA2NjM4MjEwNzc3MDg4.Xh3-gw.nV8-v9oh-UTVLeoswi15SC6ugxI'
+API_KEY = os.environ.get('API_KEY')
+CSE_ID = os.environ.get('CSE_ID')
+DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
 COMMAND_PREFIX = '!'
