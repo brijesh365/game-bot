02b28f6202d553e2e6f5d7a5d7cc2d44e3d34af9
brijesh <brijesh365@gmail.com>
2020-01-16 23:33:39 +0530

    Error handling.

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import google_search
 import settings
 from database import db
 
+INTERNAL_SERVER_ERROR = 'Internal server error.'
 SEARCH_PARAMETER_REQUIRED = 'Search parameter is mandatory!'
 EMPTY_HISTORY = 'No recent queries found matching for given keyword!'
 HELLO_RESPONSE = 'hello'
@@ -19,25 +20,29 @@ async def on_ready():
 async def on_message(message):
     if message.content.lower() == 'hi':
         await message.channel.send(HELLO_RESPONSE)
-    await bot.process_commands(message)
+    else:
+        await bot.process_commands(message)
 
 
 @bot.command(pass_context=True)
-async def google(ctx, keyword=None):
-    if keyword:
-        db.save_keyword(ctx.message.author.name, keyword)
-        await ctx.send(f'{google_search.search(keyword)}')
-    else:
-        await ctx.send(SEARCH_PARAMETER_REQUIRED)
+async def google(ctx, *, keyword):
+    db.save_keyword(ctx.message.author.name, keyword)
+    await ctx.send(f'{google_search.search(keyword)}')
 
 
 @bot.command(pass_context=True)
-async def recent(ctx, keyword=None):
-    if keyword:
-        output = db.fetch_history(ctx.message.author.name, keyword)
-        await ctx.send(output if output else EMPTY_HISTORY)
-    else:
+async def recent(ctx, *, keyword):
+    output = db.fetch_history(ctx.message.author.name, keyword)
+    await ctx.send(output if output else EMPTY_HISTORY)
+
+
+@bot.listen()
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(SEARCH_PARAMETER_REQUIRED)
+    else:
+        await ctx.send(INTERNAL_SERVER_ERROR)
+        print(str(error))
 
 
 bot.run(settings.DISCORD_TOKEN)
--- database/connection.py
+++ database/connection.py
@@ -2,31 +2,24 @@ import psycopg2
 
 import settings
 
-
-def singleton(func):
-    db_connection = None
-
-    def get_connection(*args, **kwargs):
-        if not db_connection:
-            db_connection = func(*args, **kwargs)
-        return db_connection
-
-    return get_connection
+db_connection = None
 
 
-@singleton
 def get_db_connection():
     """Gets the db connection.
 
     :return: Database connection
     """
+    global db_connection
+    if db_connection:
+        return db_connection
     try:
-        connection = psycopg2.connect(user=settings.DB_USERNAME,
-                                      password=settings.DB_PASSWORD,
-                                      host=settings.DB_HOST,
-                                      port="5432",
-                                      database=settings.DB_NAME)
-        connection.autocommit = True
-        return connection
+        db_connection = psycopg2.connect(user=settings.DB_USERNAME,
+                                         password=settings.DB_PASSWORD,
+                                         host=settings.DB_HOST,
+                                         port="5432",
+                                         database=settings.DB_NAME)
+        db_connection.autocommit = True
+        return db_connection
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
